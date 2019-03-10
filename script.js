4d3b66c4eced5c7637165bff116bcb822f3866b6
fayd <ibraimov.a.i115@gmail.com>
2019-03-10 18:24:26 +0300

    add light/dark ico, small style changes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var themeSwitcher = document.getElementById('changeTheme'),
     elem = document.querySelectorAll("#section"),
     elem1 = document.querySelectorAll(".lesson__plan"),
     elem2 = document.querySelectorAll(".lesson-block-title");
-    elem3 = document.querySelector(".logo");
+    elem3 = document.querySelectorAll(".logo");
 
 themeSwitcher.onclick = function() {
     document.body.classList.toggle('dark-theme-change');
@@ -16,5 +16,8 @@ themeSwitcher.onclick = function() {
     for (var k = 0; k < elem2.length; k++) {
         elem2[k].classList.toggle("dark-theme-text");
     }
-    elem3.classList.toggle("dark-theme-text");
+    for (var l = 0; l < elem3.length; l++) {
+        elem3[l].classList.toggle("dark-theme-text");
+    }
+    themeSwitcher.classList.toggle("dark-theme");
 }
