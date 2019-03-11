a4003f697c91b432f324e89e58b0f9098c77bab7
fayd <ibraimov.a.i115@gmail.com>
2019-03-11 21:17:17 +0300

    dark theme style changes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var themeSwitcher = document.getElementById('changeTheme'),
     elem = document.querySelectorAll("#section"),
-    elem1 = document.querySelectorAll(".lesson__plan"),
-    elem2 = document.querySelectorAll(".lesson-block-title");
-    elem3 = document.querySelectorAll(".logo");
+    elem1 = document.querySelectorAll(".lesson__plan, .lesson-block-title, .logo"),
+    elem2 = document.querySelectorAll(".lesson"),
+    elem3 = document.querySelectorAll(".lesson__title");
 
 themeSwitcher.onclick = function() {
     document.body.classList.toggle('dark-theme-change');
@@ -14,10 +14,11 @@ themeSwitcher.onclick = function() {
         elem1[j].classList.toggle("dark-theme-text");
     }
     for (var k = 0; k < elem2.length; k++) {
-        elem2[k].classList.toggle("dark-theme-text");
+        elem2[k].classList.toggle("dark-theme-lesson");
     }
     for (var l = 0; l < elem3.length; l++) {
-        elem3[l].classList.toggle("dark-theme-text");
+        elem3[l].classList.toggle("dark-theme-lesson__title");
     }
     themeSwitcher.classList.toggle("dark-theme");
 }
+
